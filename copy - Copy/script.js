424353db65c157abc891d1c9600fad9fcffc498a
Abhishek <abhikumar1260@gmail.com>
2025-08-04 14:56:02 +0530

    add new image and preloading amination

--- copy - Copy/script.js
+++ copy - Copy/script.js
@@ -26,3 +26,45 @@ document
       }
     );
   });
+
+  gsap.fromTo(
+    ".loading-page",
+    { opacity: 1 },
+    {
+      opacity: 0,
+      display: "none",
+      duration: 1.5,
+      delay: 3.5,
+    }
+  );
+
+  gsap.fromTo(
+    ".logo-name",
+    {
+      y: 50,
+      opacity: 0,
+    },
+    {
+      y: 0,
+      opacity: 1,
+      duration: 2,
+      delay: 0.5,
+    }
+  );
+
+
+  window.addEventListener("load", () => {
+    // 1. Add fade-out class after SVG drawing is done (e.g. after 2s)
+    setTimeout(() => {
+      document.getElementById("svg").classList.add("fade-out");
+    }, 2000); // or match your draw animation duration
+
+    // 2. Then hide the preloader and show main content after fade completes
+    setTimeout(() => {
+      document.querySelector(".loading-page").style.display = "none";
+      const content = document.getElementById("main-content");
+      content.classList.remove("hidden");
+      document.body.classList.remove("hidden-scroll");
+    }, 3000); // give 1s for fade-out to complete
+  });
+  
